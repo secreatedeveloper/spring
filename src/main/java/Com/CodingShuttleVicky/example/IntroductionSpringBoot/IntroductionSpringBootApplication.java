02e4210f937d467be5ac455afa9ba53fa15f935b
secreatedeveloper <vickysharma954073@gmail.com>
2025-07-03 10:25:51 +0530

    spring

---
+++ src/main/java/Com/CodingShuttleVicky/example/IntroductionSpringBoot/IntroductionSpringBootApplication.java
@@ -0,0 +1,13 @@
+package Com.CodingShuttleVicky.example.IntroductionSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Com/CodingShuttleVicky/example/IntroductionSpringBoot/IntroductionSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package Com.CodingShuttleVicky.example.IntroductionSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
