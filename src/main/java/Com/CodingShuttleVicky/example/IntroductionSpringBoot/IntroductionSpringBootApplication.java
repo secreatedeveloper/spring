d56e483a9ea9ebc21c7ef5067b9f6576efb6b3f3
secreatedeveloper <vickysharma954073@gmail.com>
2025-07-10 13:51:03 +0530

    abhay

--- src/main/java/Com/CodingShuttleVicky/example/IntroductionSpringBoot/IntroductionSpringBootApplication.java
+++ src/main/java/Com/CodingShuttleVicky/example/IntroductionSpringBoot/IntroductionSpringBootApplication.java
@@ -8,8 +8,9 @@ public class IntroductionSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IntroductionSpringBootApplication.class, args);
-		System.out.println("hello world");
+		System.out.println("ABHAY world");
 		System.out.println("Vicky Sharma");
+		System.out.println();
 	}
 
 }
